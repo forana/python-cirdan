9d0b2766dc81fe802cf1aecf0cf4581cd3225367
forana <alex@catalyze.io>
2015-07-07 15:00:49 -0500

    fixed non-wheel dists

--- setup.py
+++ setup.py
@@ -2,18 +2,20 @@
 
 from setuptools import setup, find_packages
 
-with open("requirements.txt", 'r') as file:
-    setup(name="cirdan",
-        version="1.1.0",
-        description="Decorator-based documentation generation for Falcon-based web applications.",
-        url="https://github.com/forana/python-cirdan",
-        author="Alex Foran",
-        author_email="alex@alexforan.com",
-        packages=find_packages(),
-        install_requires=file.readlines(),
-        include_package_data=True,
-        package_data={
-            'cirdan': ['default.jinja2'],
-            'canned_environments': ['*.json']
-        }
-    )
+setup(name="cirdan",
+    version="1.1.1",
+    description="Decorator-based documentation generation for Falcon-based web applications.",
+    url="https://github.com/forana/python-cirdan",
+    author="Alex Foran",
+    author_email="alex@alexforan.com",
+    packages=find_packages(),
+    install_requires=[
+        "Jinja2==2.7.3",
+        "falcon==0.3.0"
+    ],
+    include_package_data=True,
+    package_data={
+        'cirdan': ['default.jinja2'],
+        'canned_environments': ['*.json']
+    }
+)
